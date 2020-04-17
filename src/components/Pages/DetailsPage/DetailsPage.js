09432929aa7a4210e1f9f2a02ef8855e3f6daa0f
zbattaglia <zbattaglia3@gmail.com>
2020-04-16 22:00:07 -0500

    Enabled links by button clicks on pages

--- src/components/Pages/DetailsPage/DetailsPage.js
+++ src/components/Pages/DetailsPage/DetailsPage.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class DetailsPage extends Component {
+
+  // event listener to detect click of either button
+  handleClick = ( button ) => {
+    // onClick push the target route to history to route to appropriate page
+    console.log( 'Got a click on button', button )
+    this.props.history.push( button );
+  }; // end handleClick
+
   render() {
     return (
       <div className="MovieList">
         <p>Movie Details Go Here</p>
-        <button>Back to List (back to list)</button>
-        <button>Edit (to edit page)</button>
+        <button onClick={ (event) => this.handleClick( '/list' ) }>Back to List</button>
+        <button onClick={ (event) => this.handleClick( '/edit' ) }>Edit</button>
       </div>
     );
   }
--- src/components/Pages/EditPage/EditPage.js
+++ src/components/Pages/EditPage/EditPage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class EditPage extends Component {
+
+  // detect a button click
+  handleClick = ( button ) => {
+    console.log( 'Got button click', button );
+    if( button === 'save' ) {
+      console.log( 'Edits saved' );
+    }
+      this.props.history.push( '/details' );
+  } // end handleClick
+
   render() {
     return (
       <div className="MovieList">
         <p>Make Edits Here</p>
-        <button>Cancel (back to details)</button>
-        <button>Save (back to details)</button>
+        <button onClick={ (event) => this.handleClick( 'cancel' ) }>Cancel</button>
+        <button onClick={ (event) => this.handleClick( 'save' ) }>Save</button>
       </div>
     );
   }
